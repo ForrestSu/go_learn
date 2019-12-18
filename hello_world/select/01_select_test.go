e07af70005aa44abeede9f16d0a2cbd71a00ffb9
sunquan <quan.sun@xuncetech.com>
2019-12-18 20:46:23 +0800

    add select demo

---
+++ hello_world/select/01_select_test.go
@@ -0,0 +1,59 @@
+package try_select
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestEmptySelect(t *testing.T) {
+	// fatal error: all goroutines are asleep - deadlock!
+	select {}
+}
+
+func TestChannelSelect(t *testing.T) {
+	ch01 := make(chan int)
+	ch02 := make(chan int)
+	go func(ch01 chan int, ch02 chan int) {
+		fmt.Println("enter co-routine...")
+		select {
+		case rc1 := <-ch01:
+			fmt.Println("rc1", rc1)
+		case rc2 := <-ch02:
+			fmt.Println("rc1", rc2)
+		default:
+			fmt.Println("no data")
+		}
+	}(ch01, ch02)
+
+	time.Sleep(time.Second)
+	close(ch01)
+	// panic: send on closed channel
+	ch01 <- 100
+}
+
+// 采用 select 实现超时
+func TestSelectTimeOut(t *testing.T) {
+	ch01 := make(chan int)
+	go func(ch01 chan int) {
+		fmt.Println("enter co-routine...")
+		isRun := true
+		for isRun {
+			select {
+			case rc1, ok := <-ch01:
+				fmt.Println("rc1", rc1)
+				if ok == false {
+					isRun = false
+					fmt.Println("channel closed")
+				}
+			case rc2 := <-time.After(time.Millisecond * 200):
+				fmt.Println("time out 1 sec", rc2)
+			}
+		}
+		fmt.Println("exit co-routine...")
+	}(ch01)
+	time.Sleep(time.Second * 2)
+	// 2秒之后发送一个消息
+	ch01 <- 10
+	close(ch01)
+}
