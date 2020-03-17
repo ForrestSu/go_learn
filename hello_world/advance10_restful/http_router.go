68fbb7752ecf5b14079096a0dd80f05dfa0f7bdc
sunquan <quan.sun@xuncetech.com>
2020-03-17 18:06:23 +0800

    [http] add http_router demo!

---
+++ hello_world/advance10_restful/http_router.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "fmt"
+    "github.com/julienschmidt/httprouter"
+    "log"
+    "net/http"
+)
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+    _,_ = fmt.Fprint(w, "Welcome!\n")
+}
+
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+    _,_ = fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+}
+
+func main() {
+    router := httprouter.New()
+    router.GET("/", Index)
+    router.GET("/hello/:servce", Hello)
+
+    log.Println("bind address: 8001")
+    err := http.ListenAndServe(":8001", router)
+    log.Fatal(err)
+}
