2bb8b89a7bc51fc82d025c7228b61ea04b6696a2
sunquan <quan.sun@xuncetech.com>
2020-04-08 14:19:58 +0800

    test sync.Pool!

--- hello_world/advance07_syncPool/sync_pool_test.go
+++ hello_world/advance07_syncPool/sync_pool_test.go
@@ -16,3 +16,28 @@ func TestSyncPool(t *testing.T) {
 	obj, _ := pool.Get().(int)
 	t.Log(obj)
 }
+
+/**
+output:
+1
+4
+3
+2
+0
+*/
+func TestSyncPoolV2(t *testing.T) {
+	p := sync.Pool{
+		New: func() interface{} {
+			return 0
+		},
+	}
+	p.Put(1)
+	p.Put(2)
+	p.Put(3)
+	p.Put(4)
+
+	//runtime.GC()
+	for i := 0; i < 5; i++ {
+		fmt.Println(p.Get())
+	}
+}
