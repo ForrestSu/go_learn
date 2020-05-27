b4680c6907bce192e39f3883a55f6577bf106327
sunquan <quan.sun@xuncetech.com>
2020-05-27 19:00:27 +0800

    add flag!

---
+++ hello_world/advance00_flag/flag.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "flag"
+    "fmt"
+)
+
+var name = flag.String("name", "Tom", "Input your name")
+var age = flag.Int("age", 18, "Input your age")
+var f = flag.Bool("isVIP", false, "Is VIP")
+var postCode int
+
+func init() {
+    flag.IntVar(&postCode, "postcode", 1234, "Input your post code")
+}
+
+func main() {
+    flag.Parse()
+    fmt.Println("name:", *name)
+    fmt.Println("age:", *age)
+    fmt.Println("VIP:", *f)
+    fmt.Println("postCode:", postCode)
+}
+
+
