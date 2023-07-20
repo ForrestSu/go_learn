bb1a5fc11980ba47665acac28a4acfdce7bf4d5d
sunquana <sunquana@gmail.com>
2023-07-20 10:40:02 +0800

    feat: http server

--- 3rd-libs/10_http/http_server.go
+++ 3rd-libs/10_http/http_server.go
@@ -1,17 +1,21 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, request *http.Request) {
-		fmt.Fprint(w, "hello world!")
-	})
+	var path = "."
+	var localAddr = "localhost:8000"
+	flag.StringVar(&path, "path", ".", "PATH")
+	flag.Parse()
 
-	if err := http.ListenAndServe("localhost:8082", nil); err != nil {
+	http.Handle("/", http.FileServer(http.Dir(path)))
+	fmt.Printf("visit http://%s ... path=%s\n", localAddr, path)
+	if err := http.ListenAndServe(localAddr, nil); err != nil {
 		log.Fatal(err)
 	}
 }
