1696fda7a0363a00d2d11c13285a8142e4ec2ef9
sunquana <sunquana@gmail.com>
2024-04-10 10:20:34 +0800

    feat: hkd展示卖出价

--- tools/hkd/model.go
+++ tools/hkd/model.go
@@ -27,22 +27,22 @@ func (r *FxReply) Find(name string) *FxRate {
 }
 
 type FxRate struct {
-	Name   string `json:"ccyNbr"` // 币种
-	MidPx  string `json:"rtbBid"` // 中间价
-	RthOfr string `json:"rthOfr"` // 现汇卖出
-	RtcOfr string `json:"rtcOfr"` // currency
-	RthBid string `json:"rthBid"` // 现汇买入
-	RtcBid string `json:"rtcBid"` // currency
-	Time   string `json:"ratTim"`
-	Date   string `json:"ratDat"`
+	Name     string `json:"ccyNbr"` // 币种
+	MidPx    string `json:"rtbBid"` // 中间价
+	RthOffer string `json:"rthOfr"` // 现汇卖出(银行卖出)
+	RtcOffer string `json:"rtcOfr"` // 现钞卖出
+	RthBid   string `json:"rthBid"` // 现汇买入
+	RtcBid   string `json:"rtcBid"` // 现钞买入
+	Time     string `json:"ratTim"`
+	Date     string `json:"ratDat"`
 }
 
 // 字符串替换器
 var replacer = strings.NewReplacer("港币", "HKD")
 
 func (r *FxRate) String() string {
-	return fmt.Sprintf(" %s Rate: %s (%s %s)",
-		replacer.Replace(r.Name), utils.TitlePt.Sprint(r.MidPx), r.Time, r.Date)
+	return fmt.Sprintf(" %s MidRate: %s 现汇卖出: %s (%s %s)",
+		replacer.Replace(r.Name), utils.TitlePt.Sprint(r.MidPx), utils.InfoPt.Sprint(r.RthOffer), r.Date, r.Time)
 }
 
 // MidPrice 汇率-中间价
