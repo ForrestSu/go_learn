1bd908103a277bfbccddc3e02c8b5429187cb2d9
sunquana <sunquana@gmail.com>
2025-06-20 17:12:18 +0800

    feat: upgrade hkd

--- tools/hkd/model.go
+++ tools/hkd/model.go
@@ -30,8 +30,8 @@ type FxRate struct {
 	Name     string `json:"ccyNbr"` // 币种
 	MidPx    string `json:"rtbBid"` // 中间价
 	RthOffer string `json:"rthOfr"` // 现汇卖出(银行卖出)
-	RtcOffer string `json:"rtcOfr"` // 现钞卖出
 	RthBid   string `json:"rthBid"` // 现汇买入
+	RtcOffer string `json:"rtcOfr"` // 现钞卖出
 	RtcBid   string `json:"rtcBid"` // 现钞买入
 	Time     string `json:"ratTim"`
 	Date     string `json:"ratDat"`
@@ -41,8 +41,9 @@ type FxRate struct {
 var replacer = strings.NewReplacer("港币", "HKD")
 
 func (r *FxRate) String() string {
-	return fmt.Sprintf(" %s MidRate: %s 现汇卖出: %s (%s %s)",
-		replacer.Replace(r.Name), utils.TitlePt.Sprint(r.MidPx), utils.InfoPt.Sprint(r.RthOffer), r.Date, r.Time)
+	return fmt.Sprintf(" %s MidRate: %s 现汇卖出: %s 现汇买入: %s (%s %s)",
+		replacer.Replace(r.Name), utils.TitlePt.Sprint(r.MidPx),
+		utils.InfoPt.Sprint(r.RthOffer), r.RthBid, r.Date, r.Time)
 }
 
 // MidPrice 汇率-中间价
